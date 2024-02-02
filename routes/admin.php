71a45ad05d03e646fc7c8af65be9d7f236c35f82
lrivas12 <116288060+lrivas12@users.noreply.github.com>
2024-02-02 12:15:52 -0600

    mejorar rutas

--- routes/admin.php
+++ routes/admin.php
@@ -30,10 +30,6 @@ use App\Http\Controllers\VentaControlller;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/usuario', (UsuarioController::class))->middleware("Roles:Administrador");
